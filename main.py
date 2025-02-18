265f32b139cacaf4651e28e29e80232eb3ae457a
Gazes <gazesprogmetal@gmail.com>
2025-02-18 10:51:23 +0100

    player rotation and movement

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        player.draw(screen)
+        player.update(dt)
+        dt = clock.tick(60) / 1000
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
